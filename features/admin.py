9d592cf648aee9c836298925df5921444edb7f17
Roshan Dahal <roshandahal.r@gmail.com>
2023-04-15 22:59:24 +0545

    sdad

--- features/admin.py
+++ features/admin.py
@@ -12,11 +12,51 @@ class BlogField(SummernoteModelAdmin):
 class HomeFields(SummernoteModelAdmin):
     summernote_fields = ('sub_text','intro_text','message')
 
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        else:
+            return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 class AboutFields(SummernoteModelAdmin):
     summernote_fields = ('sub_text','mission','vision')
+    
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        else:
+            return True
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class AddSingle(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        else:
+            return True
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+class CompanySetupAdmin(SummernoteModelAdmin):
+    summernote_fields = ('terms_and_conditions', 'privacy_policy')
+
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        else:
+            return True
+        
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(SchoolSetup, CompanySetupAdmin)
 admin.site.register(Blog,BlogField)
-admin.site.register(SchoolSetup)
 admin.site.register(TeamMember)
 admin.site.register(Testimonial)
 admin.site.register(Faqs)
@@ -27,10 +67,11 @@ admin.site.register(AboutContent,AboutFields)
 
 
 
+
+
+
+
 GalleryImageFormSet = inlineformset_factory(Gallery, GalleryImage, form=GalleryImageForm, extra=1, can_delete=True)
-
-
-
 class GalleryImageInline(admin.TabularInline):
     model = GalleryImage
     formset = GalleryImageFormSet
